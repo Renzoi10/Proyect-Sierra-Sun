100bd95360578a12ec3408ecd0b59d54d204dee9
Renzo <renzitofabri@hotmail.com>
2020-06-20 18:02:50 -0500

    Se modifico la tabla para implementar el CRUD

--- assets/js/tables.js
+++ assets/js/tables.js
@@ -19,3 +19,21 @@ $(document).ready(function() {
   } );
 } );
 /*Tables scripts end*/
+
+/*Scripsts boutons tables validations*/
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    var forms = document.getElementsByClassName('needs-validation');
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+/*Scripsts boutons tables validations end*/

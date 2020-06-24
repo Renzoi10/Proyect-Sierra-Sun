af2457bf896dd1c952218e5ab867691220f5dc68
Ronald <65217938+Ronald-QH@users.noreply.github.com>
2020-06-24 07:13:29 -0500

    Update tables.js
    Se Agrego una nueva funcionalidad para  el apartado del Tablero para el registro de Trabajadores del Apartado del Administrador.

--- assets/js/tables.js
+++ assets/js/tables.js
@@ -1,21 +1,43 @@
 /*Tables scripts*/
 $(document).ready(function() {
-  $('#table1').DataTable( {
-      "language": {
-          "lengthMenu": "Numero de datos para visualizar _MENU_ ",
-          "zeroRecords": "No se encontro elemento",
-          "info": "Pagina _PAGE_ de _PAGES_",
-          "infoEmpty": "No hay datos disponibles",
-          "infoFiltered": "(Se econtraron _TOTAL_ elementos de _MAX_ )",
-          "sSearch": "Buscar",
-          "oPaginate": {
-            "sFirst": "Primero",
-            "sLast": "Ultimo",
-            "sNext": "Siguiente",
-            "sPrevious": "Anterior"
-          },
-          "sProcessing": "Procesando....",
-      }
-  } );
-} );
+  $('#table1').DataTable({
+    "language": {
+      "lengthMenu": "Numero de datos para visualizar _MENU_ ",
+      "zeroRecords": "No se encontro elemento",
+      "info": "Pagina _PAGE_ de _PAGES_",
+      "infoEmpty": "No hay datos disponibles",
+      "infoFiltered": "(Se econtraron _TOTAL_ elementos de _MAX_ )",
+      "sSearch": "Buscar",
+      "oPaginate": {
+        "sFirst": "Primero",
+        "sLast": "Ultimo",
+        "sNext": "Siguiente",
+        "sPrevious": "Anterior"
+      },
+      "sProcessing": "Procesando....",
+    }
+  });
+});
+
+
+$(document).ready(function() {
+  $('#table2').DataTable({
+    "language": {
+      "lengthMenu": "Numero de datos para visualizar MENU ",
+      "zeroRecords": "No se encontro elemento",
+      "info": "Pagina PAGE de PAGES",
+      "infoEmpty": "No hay datos disponibles",
+      "infoFiltered": "(Se econtraron TOTAL elementos de MAX )",
+      "sSearch": "Buscar",
+      "oPaginate": {
+        "sFirst": "Primero",
+        "sLast": "Ultimo",
+        "sNext": "Siguiente",
+        "sPrevious": "Anterior"
+      },
+      "sProcessing": "Procesando....",
+    }
+  });
+});
+
 /*Tables scripts end*/
